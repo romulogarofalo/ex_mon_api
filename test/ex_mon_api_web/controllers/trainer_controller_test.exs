976f39442a83575eddf513ca50fcd648c32b71cc
romulo garofalo <romulogarofalo@gmail.com>
2020-09-28 08:05:19 +0000

    fixed tests of Trainer controller

--- test/ex_mon_api_web/controllers/trainer_controller_test.exs
+++ test/ex_mon_api_web/controllers/trainer_controller_test.exs
@@ -7,15 +7,27 @@ defmodule ExMonApi.Controllers.TrainerControllerTest do
     test "when there is a transfer with the given id, returns the trainer", %{conn: conn} do
       params = %{name: "romulo", password: "123123"}
 
-      {:ok, %Trainer{id: id} = ExMonApi.create_trainer(params)}
+      {:ok, %Trainer{id: id}} = ExMonApi.create_trainer(params)
 
       response =
         conn
         |> get(Routes.trainers_path(conn, :show, id))
         |> json_response(:ok)
 
-      assert response == "Trainer Created"
+      assert %{"id" => _id, "inserted_at" => inserted_at, "name" => "romulo"} = response
 
+    end
+
+    test "when there is an error, returns the error", %{conn: conn} do
+
+      response =
+        conn
+        |> get(Routes.trainers_path(conn, :show, "123"))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid ID format!"}
+
+      assert expected_response == response
     end
   end
 
