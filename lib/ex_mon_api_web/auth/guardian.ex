d81c3451334435c019b9a11f076a11ecd4672385
romulo garofalo <romulogarofalo@gmail.com>
2020-09-30 02:48:54 +0000

    added login functions

--- lib/ex_mon_api_web/auth/guardian.ex
+++ lib/ex_mon_api_web/auth/guardian.ex
@@ -1,6 +1,8 @@
 defmodule ExMonApiWeb.Auth.Guardian do
   use Guardian, otp_app: :ex_mon_api
 
+  alias ExMonApi.{Repo, Trainer}
+
   def subject_for_token(resource, _claims) do
     sub = to_string(resource.id)
     {:ok, sub}
@@ -17,4 +19,24 @@ defmodule ExMonApiWeb.Auth.Guardian do
   # def resource_from_claims(_claims) do
   #   {:error, :reason_for_error}
   end
+
+  def authenticate(%{"id" => trainer_id, "password" => password}) do
+    case Repo.get(Trainer, trainer_id) do
+      nil -> {:error, "Trainer not found"}
+      trainer -> validate_password(trainer, password)
+    end
+  end
+
+  defp validate_password(%Trainer{password_hash: hash} = trainer, password) do
+    case Argon2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, :unauthorized}
+    end
+  end
+
+  defp create_token(trainer) do
+    {:ok, token, _claims} = encode_and_sign(trainer)
+    {:ok, token}
+  end
+
 end
